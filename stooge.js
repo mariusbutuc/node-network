50c7d1ab505dbb6e1ba64559f81bd9d4bf1dae97
Marius Butuc <marius.butuc@gmail.com>
2013-02-06 17:41:55 -0500

    Backbone Model

---
+++ stooge.js
@@ -0,0 +1,13 @@
+Stooge = Backbone.Model.extend({
+  defaults: {
+    'name': 'Guy Incognito',
+    'power': 'Classified',
+
+    'friends': [],
+  },
+
+  initialize: function() {}
+    // Do initialization
+});
+
+var account = new Stooge({ name: 'Larry', power: 'Baldness', fiends: ['Curly', 'Moe']});
